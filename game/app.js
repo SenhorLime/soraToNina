de0473cf583fb45acc7586d6db4d2dfc5a493706
Limão <108885897+SenhorLime@users.noreply.github.com>
2022-11-25 00:56:21 -0300

    Mais Mudanças

---
+++ game/app.js
@@ -0,0 +1,172 @@
+// Importando as questoes, respostas, resposta correta e localizacao das imagens
+import { quizData } from "./quizData.js";
+
+// Declaracao de variaveis para localizacao de partes do HTML 
+const questionScene = document.querySelector(".quiz-scene");
+const quiz = document.querySelector(".quiz-container");
+const questionElement = document.querySelector(".quiz-header");
+const answerElements = document.querySelectorAll(".answer");
+const questionNumber = document.querySelector("title");
+
+// Declaracao de variaveis para localizacao das respostas
+const a_text = document.getElementById("text_a");
+const b_text = document.getElementById("text_b");
+const c_text = document.getElementById("text_c");
+const d_text = document.getElementById("text_d");
+
+// Botao para mudar a pergunta
+const submitButton = document.getElementById("submit");
+
+// Variaveis para o timer
+const display = document.querySelector(".timer");
+let timer;
+
+// Variaveis que contam qual a questao atual e quantos pontos marcados
+let currentQuiz = 0;
+let score = 0;
+
+
+// Funcao para deselecionar as repostas
+const deselectAnswers = () => { // Inicio da funcao
+  answerElements.forEach((answer) => (answer.checked = false));
+};
+// Fim da funcao
+
+
+// Funcao para armazenar qual resposta foi marcada
+const getSelected = () => { // Inicio da funcao
+  let answer;
+
+  answerElements.forEach((answerElements) => {
+    if (answerElements.checked) {
+      answer = answerElements.id;
+    }
+  });
+
+  return answer;
+};
+// Fim da funcao
+
+
+// Funcao que carrega a pergunta e as respostas do quiz
+const loadQuiz = () => { // Inicio da funcao
+  deselectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  questionNumber.innerHTML = `STN - Fase ${currentQuiz + 1}`; // Muda o titulo da pagina de acordo com o numero da questao
+  questionScene.innerHTML = `<img src="${currentQuizData.scene}"/>`; // Troca a imagem da cena de acordo com a cena
+  questionElement.innerText = currentQuizData.question; // Carrega a pergunta 
+
+  // Carrega as repostas para a pergunta
+  a_text.innerText = currentQuizData.answer.a;
+  b_text.innerText = currentQuizData.answer.b;
+  c_text.innerText = currentQuizData.answer.c;
+  d_text.innerText = currentQuizData.answer.d;
+
+  // Exibe qual questao e quantas ja foram marcadas no console (Usado na fase de testes, mas acho que e interessante entao decidi nao tirar)
+  console.log("Questão atual: ", currentQuiz + 1);
+  console.log("Quantidade de acertos: ", score);
+};
+// Fim da funcao
+
+loadQuiz(); // Carrega o quiz
+
+
+// Funcao ativada a partir do evento onclick no botao submit
+submitButton.addEventListener("click", () => { // Inicio da funcao
+  console.clear(); // Limpa o console para nao deixar poluido
+
+
+  const answer = getSelected(); // Pega a resposta marcada no input
+
+  // Realizacao de testes para saber se a reposta marcada foi certa ou errada
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) { 
+      score++; // Aumenta o contador desde que a reposta correta seja marcada
+    }
+    currentQuiz++; // Aumenta o contador de qual questao esta
+
+    timer = 60; // Define o tempo para o timer
+
+    // Verifica se ainda existem questoes para serem respondidas
+    if (currentQuiz < quizData.length) { 
+      loadQuiz(); // Se sim carrega a proxima
+    } else {
+
+      // Senao exibe a tela final e define o tempo do timer para algo que nao vai ser alcancado
+      timer = 9999999;
+
+      if (score < quizData.length / 2) {
+        quiz.innerHTML = `
+                <div class="final-score">
+                    <h2>Você acertou ${score}/${quizData.length} perguntas</h2>
+                    <p>
+                        Infelizmente Sora não conquistou o coração de Nina e voltou embora para casa triste
+                    </p>
+                </div>
+
+                <div class="control-panel">
+                    <button>
+                        <a href="/credits/index.html">Créditos</a>
+                    </button>
+                    <button onclick="history.go(0)">Jogar Novamente</button>
+                </div>`;
+      } else {
+        quiz.innerHTML = `
+                <div class="final-score">
+                    <h2>Você acertou ${score}/${quizData.length} perguntas</h2>
+                    <p>
+                        Sora conseguiu se declarar para Nina e voltou para casa feliz
+                    </p>
+                </div>
+
+                <div class="control-panel">
+                    <button>
+                        <a href="/credits/index.html">Créditos</a>
+                    </button>
+                    <button onclick="history.go(0)">Jogar Novamente</button>
+                </div>`;
+      }
+    }
+  }
+});
+
+
+// Funcao que inicia o timer
+function startTimer(duration, display) {
+  let minutes, seconds;
+  timer = duration;
+
+  setInterval(function () {
+    //funcao que roda a cada 1 seg
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes; //adicionar 00 nos locais certos
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds; //adicionar os dois pontos entre minutos e segundos
+
+    if (--timer < 0) {
+      timer = 0;
+      window.alert(
+        "O tempo para responder acabou. Clique em ok para continuar a jogar"
+      );
+
+      currentQuiz++;
+      timer = 60;
+
+      if (currentQuiz < quizData.length) {
+        loadQuiz();
+      } else {
+        timer = 9999999;
+      }
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var time = 60;
+  startTimer(time, display);
+};
---
+++ game/quizData.js
@@ -0,0 +1,182 @@
+export const quizData = [
+  {
+    question:
+      "Sora é um(a) estudante do ensino médio normal. Ele começa mais um dia escolar. Se levanta e vai tomar um banho gelado para se despertar. Qual hormônio liberado?",
+    answer: {
+      a: "Prolactina",
+      b: "Cortisol",
+      c: "Testoterona",
+      d: "Adrenalina",
+    },
+    correct: "b",
+    scene: "/Assets/Scenes/soraBath.gif",
+  },
+ {
+    question:
+      "Logo após o banho sora vai para a cozinha e se depara com sua comida preferida. Qual o hormônio liberado?",
+    answer: {
+      a: "Tiroxina",
+      b: "Adrenalina",
+      c: "Serotonina",
+      d: "Glucagon",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/soraEating.gif",
+  },
+  {
+    question: 
+      "Sora pega seu skate, põe seu capacete e vai em direção a escola, mas no caminho um motorista imprudente avança o sinal vermelho e quase atropela sora. Qual o hormônio liberado?",
+    answer: {
+      a: "Estrogênio",
+      b: "Paratormônio",
+      c: "Testoterona",
+      d: "Cortisol",
+    },
+    correct: "d",
+    scene: "/Assets/Scenes/soraSkating.gif",
+  },
+  {
+    question:
+      "Após esse susto Sora continua seu caminho até a escola. Chegando a escola, ele encontra com o professor que está devendo uma atividade, com medo, tenta se esconder. Qual hormônio liberado?",
+    answer: {
+      a: "Insulina",
+      b: "Epinefrina",
+      c: "Adrenalina",
+      d: "TSH",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/runningInThe90s.gif",
+  },
+  {
+    question:
+      "Indo em direção a sala de aula, Sora se depara com sua Crush, Nina. Sora a olha de longe apaixonado. Qual hormônio liberado?",
+    answer: {
+      a: "Testoterona",
+      b: "Dopamina",
+      c: "Norepinefrina",
+      d: "ACTH",
+    },
+    correct: "b",
+    scene: "/Assets/Scenes/soraLove.gif",
+  },
+  {
+    question:
+      "Dentro da sala de aula, Sora está tendo a aula mais chata de todas. Ele começa a ficar com sono e dá uma cochilada durante a aula. Qual hormônio causou isso?",
+    answer: {
+      a: "LH",
+      b: "Melatonina",
+      c: "Calcitonina",
+      d: "Glicocorticoides",
+    },
+    correct: "b",
+    scene: "/Assets/Scenes/soraCochilinho.gif",
+  },
+  {
+    question:
+      "Sora acorda assustado com o sinal do intervalo. Esse susto foi causado por qual hormônio?",
+    answer: {
+      a: "Dopamina",
+      b: "Profesterona",
+      c: "Paratormônio",
+      d: "Noradrenalina",
+    },
+    correct: "d",
+    scene: "/Assets/Scenes/soraWakeUp.gif",
+  },
+  {
+    question:
+      "Ao se levantar Sora percebe que não havia tomado água nesse dia ainda e sente que sua garganta está seca. Qual o hormônio liberado?",
+    answer: {
+      a: "GH",
+      b: "Melatonina",
+      c: "Antidiúretico",
+      d: "Mineralocorticoídes",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/soraAguinha.gif",
+  },
+  {
+    question:
+      "Depois de tomar sua aguinha, Sora vai em direção a sala para estudar. No próximo horário tem uma prova e ele está nervoso porque não estudou. Qual hormônio liberado?",
+    answer: {
+      a: "Insulina",
+      b: "Cortisol",
+      c: "Estrogênio",
+      d: "Testoterona",
+    },
+    correct: "b",
+    scene: "/Assets/Scenes/soraProva.gif",
+  },
+  {
+    question:
+      "A hora da prova chegou! Sora está com medo por não saber a matéria. Seu corpo começa a produzir Cortisol e Adrenalina sem parar. O que está produzindo esses hormônios?",
+    answer: {
+      a: "Glândula Pineal",
+      b: "Tireoide",
+      c: "Grândulas Suprarenais",
+      d: "Pâncreas",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/soraProva.gif",
+  },
+  {
+    question:
+      "Durante a prova Sora fica com medo ao ver seu colega colando e fica com medo de também levar a culpa se seu colega for pego. Qual o hormônio liberado?",
+    answer: {
+      a: "Estrogênio",
+      b: "Dopamina",
+      c: "Noradrenalina",
+      d: "Mineralocorticoídes",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/street.jpeg",
+  },
+  {
+    question:
+      "Fazendo a prova, Sora se depara com as seguintes questões. 1) qual dos órgãos a seguir não participa do sistema endócrino? ",
+    answer: {
+      a: "Hipófise",
+      b: "Rins",
+      c: "Fígado",
+      d: "Nenhuma Alternativa",
+    },
+    correct: "d",
+    scene: "/Assets/Scenes/corpoHumano.jpeg",
+  },
+  {
+    question:
+      "2)Localizados nos rins, os néfrons são a unidade funcional dos mesmos. Qual dos seguintes não corresponde a uma parte dos néfrons?",
+    answer: {
+      a: "Corpúsculo Renal",
+      b: "Ducto Deferente",
+      c: "Ducto Coletor",
+      d: "Cápsula Renal",
+    },
+    correct: "b",
+    scene: "/Assets/Scenes/corpoHumano.jpeg",
+  },
+  {
+    question:
+      "3)Os principais hormônios femininos são o _________ e a ________, fabricados nos, entram em atividade na adolescência e sofrem variações constantes durante o dia a dia da mulher. A opção que melhor preenche as lacunas é:",
+    answer: {
+      a: "Estrogênio e Prolactina",
+      b: "Testoterona e Cortisol",
+      c: "Estrogênio e Progesterona",
+      d: "ADH e Insulina",
+    },
+    correct: "c",
+    scene: "/Assets/Scenes/corpoHumano.jpeg",
+  },
+  {
+    question:
+      "4)A hipófise possui importantes funções, como controlar outras glândulas e contribuir para o metabolismo e produção de hormônios. A partir de sua relação com o ___________, a hipófise representa locais de interação entre o sistema endócrino e o sistema _________. Preenchendo as lacunas, encontramos:",
+    answer: {
+      a: "Hipotálamo - Nervoso",
+      b: "Mio Cárdio - Circulatório",
+      c: "Hipófise - Digestório",
+      d: "Bulbo - Digestório",
+    },
+    correct: "a",
+    scene: "/Assets/Scenes/corpoHumano.jpeg",
+  },
+];
